2e4875e5e824f30c2087106c76c254ab35c14d4d
Lucas Teodosio (Lusca) <lucasfrofc@gmail.com>
2025-04-06 15:14:49 -0300

    Add files via upload

--- Exp2/CodeAnalysis.py
+++ Exp2/CodeAnalysis.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
+from scipy.stats import linregress
 
 # Carregar os dados 
 # data = np.loadtxt(r'C:\Users\lucas\Downloads\planilha_exp2 - Página1.csv', delimiter=',', skiprows=1) # carregar csv
@@ -20,40 +21,62 @@ R = df['R (ohms)'].astype(str).str.replace(',', '.').astype(float) # resistênci
 inv_R = 1/R
 # print(Vripple)
 # print(R)
-
 '''
-# Criar um gráfico de dispersão dos dados
-plt.figure(figsize=(8, 6))
-plt.scatter(inv_R, Vripple, color='blue', label='Dados experimentais', alpha=0.6) # X = freq; Y = T;
-plt.xlabel('Variável Independente (R)')
-plt.ylabel('Variável Dependente (Vripple)')
-plt.title('Análise de Dados com PyLab')
-plt.legend()
-plt.grid(True)
-plt.show()
-'''
-#### AJUSTE EXPONENCIAL ########
-# Define o modelo de ajuste
-def modelo(x, Vp, k):
-    return Vp * (1 - np.exp(-k * x)) # k = t/C x = 1/R
+##### AJUSTE EXPONENCIAL (COM LINEARIZAÇÃO) ########
+# Converte Vr para um array do NumPy
+Vr_array = np.array(Vripple)
 
-t = 1/60
-# Chute inicial para Vp e RC
-p0 = [max(Vripple), 1.0]
+# Aplica a equação em todos os elementos
+V0 = 14.7 # escrever valor
+resultado = np.log(np.abs((V0 - Vr_array) / V0))
 
-params, _ = curve_fit(modelo, inv_R, Vripple, p0=p0)
-Vp_fit, k_fit = params
+print(resultado)
+resultado_lista = resultado.tolist()
 
-print(f'Vp ajustado: {Vp_fit:.4f} V')
-print(f'k ajustado: {k_fit:.4f} s')
+# FAZENDO A REGRESSÃO
+# Dados
+X = inv_R
+Y = resultado_lista
 
-x_fit = np.linspace(min(inv_R), max(inv_R), 200)
-y_fit = modelo(x_fit, Vp_fit, k_fit)
+# Regressão linear
+slope, intercept, r_value, p_value, std_err = linregress(X, Y)
 
-t = 1/60  # tempo fixo em segundos
-C = t / k_fit
+print(f"Inclinação (slope): {slope}")
+print(f"Intercepto: {intercept}")
+print(f"R²: {r_value**2}")
+
+t = 1/120  # tempo fixo em segundos
+# slope = t/C
+C = - t / slope
 
 print(f"Capacitância estimada: {C:.6f} F") # checar valor da capacitância
+'''
+
+#'''
+#### AJUSTE EXPONENCIAL ########
+# Define o modelo de ajuste
+def modelo(x, C):
+    t = 1/120 
+    Vp = 14.7
+    return Vp * (1 - np.exp(-(t/C) * x)) # k = t/C x = 1/R
+
+#t = 1/120
+# Chute inicial para Vp e RC
+p0 = [1.0]
+
+params, _ = curve_fit(modelo, inv_R, Vripple, p0=p0)
+C_fit = params[0]
+
+#print(f'Vp ajustado: {Vp_fit:.4f} V')
+#print(f'k ajustado: {k_fit:.4f} s')
+
+x_fit = np.linspace(min(inv_R), max(inv_R), 200)
+y_fit = modelo(x_fit, C_fit)
+
+#t = 1/120  # tempo fixo em segundos
+#C = t / k_fit
+
+print(f"Capacitância estimada: {C_fit:.6f} F") # checar valor da capacitância
 
 
 plt.scatter(inv_R, Vripple, label='Dados experimentais')
@@ -65,3 +88,4 @@ plt.grid(True)
 plt.title('Ajuste da função exponencial')
 plt.show()
 
+#'''
